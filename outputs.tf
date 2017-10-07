231dc9f9ce9764b3dccbf08f0063a5da890a40da
Koe <koe.systems@gmail.com>
2017-10-07 03:38:22 +0200

    Add descriptions to variables and outputs

--- outputs.tf
+++ outputs.tf
@@ -1,18 +1,18 @@
-output "vpc_id"                     { value = "${aws_vpc.mod.id}" }
-output "vpc_name"                   { value = "${var.name}" }
-output "ipv4_cidr_block"            { value = "${aws_vpc.mod.cidr_block}" }
-output "ipv6_cidr_block"            { value = "${aws_vpc.mod.ipv6_cidr_block }" }
-output "domain_name"                { value = "${var.domain_name}" }
-output "nat_ids"                    { value = "${join(",", aws_eip.mod.*.id) }" }
-output "nat_private_ips"            { value = "${join(",", aws_eip.mod.*.private_ip) }" }
-output "nat_public_ips"             { value = "${join(",", aws_eip.mod.*.public_ip) }" }
-output "subnets_private_ids"        { value = "${join(",", aws_subnet.private.*.id) }" }
-output "subnets_private_cidr_block" { value = "${join(",", aws_subnet.private.*.cidr_block) }" }
-output "subnets_public_ids"         { value = "${join(",", aws_subnet.public.*.id) }" }
-output "subnets_public_cidr_block"  { value = "${join(",", aws_subnet.public.*.cidr_block) }" }
-output "rtb_private_ids"            { value = "${join(",", aws_route_table.private.*.id) }" }
-output "rtb_public_ids"             { value = "${join(",", aws_route_table.public.*.id) }" }
-output "route_private_rtb_ids"      { value = "${join(",", aws_route_table.private.*.route_table_id) }" }
-output "route_public_rtb_ids"       { value = "${join(",", aws_route_table.public.*.route_table_id) }" }
-output "secondary_public_zone_id"   { value = "${aws_route53_zone.secondary_public.zone_id}" }
-output "secondary_private_zone_id"  { value = "${aws_route53_zone.secondary_private.zone_id}" }
+output "vpc_id"                     { value = "${aws_vpc.mod.id}"                                       description = "VPC unique ID." }
+output "vpc_name"                   { value = "${var.name}"                                             description = "VPC name." }
+output "ipv4_cidr_block"            { value = "${aws_vpc.mod.cidr_block}"                               description = "IPv4 assigned to the VPC." }
+output "ipv6_cidr_block"            { value = "${aws_vpc.mod.ipv6_cidr_block }"                         description = "IPv6 assigned to the VPC." }
+output "domain_name"                { value = "${var.domain_name}"                                      description = "Route53 internal hosted zone assigned to the VPC." }
+output "nat_ids"                    { value = "${join(",", aws_nat_gateway.mod.*.id) }"                 description = "List of the NAT Gateway IDs." }
+output "nat_private_ips"            { value = "${join(",", aws_nat_gateway.mod.*.private_ip) }"         description = "List of the NAT Gateway private IPs." }
+output "nat_public_ips"             { value = "${join(",", aws_nat_gateway.mod.*.public_ip) }"          description = "List of the NAT Gateway public IPs." }
+output "subnets_private_ids"        { value = "${join(",", aws_subnet.private.*.id) }"                  description = "List of the VPC private subnets IDs." }
+output "subnets_private_cidr_block" { value = "${join(",", aws_subnet.private.*.cidr_block) }"          description = "List of the VPC private CIDR blocks." }
+output "subnets_public_ids"         { value = "${join(",", aws_subnet.public.*.id) }"                   description = "List of the VPC public subnets IDs." }
+output "subnets_public_cidr_block"  { value = "${join(",", aws_subnet.public.*.cidr_block) }"           description = "List of the VPC public CIDR blocks." }
+output "rtb_private_ids"            { value = "${join(",", aws_route_table.private.*.id) }"             description = "List of the VPC Routing Tables private IDs." }
+output "rtb_public_ids"             { value = "${join(",", aws_route_table.public.*.id) }"              description = "List of the VPC Routing Tables public IDs." }
+output "route_private_rtb_ids"      { value = "${join(",", aws_route_table.private.*.route_table_id) }" description = "List of the VPC Routes private IDs." }
+output "route_public_rtb_ids"       { value = "${join(",", aws_route_table.public.*.route_table_id) }"  description = "List of the VPC Routes public IDs." }
+output "secondary_public_zone_id"   { value = "${aws_route53_zone.secondary_public.zone_id}"            description = "Route53 public hosted zone ID." }
+output "secondary_private_zone_id"  { value = "${aws_route53_zone.secondary_private.zone_id}"           description = "Route53 private hosted zone ID." }
--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
-variable "name"                 { }
-variable "domain_name"          { }
-variable "cidr_block"           { default = "10.0.0.0/16"}
-variable "tenancy"              { default = "default" }
-variable "enable_dns_support"   { default = true }
-variable "enable_dns_hostnames" { default = true }
-variable "ipv6_cidr_block"      { default = false }
-variable "domain_name_servers"  { default = [ "AmazonProvidedDNS" ] }
-variable "AZs"                  { default = 3 }
-variable "enable_vpc_flow_logs" { default = false }
-variable "enable_nat_gw"        { default = true }
+variable "name"                 {                                       description = "AWS VPC name. This would be used as prefix in all the resources to support multiple VPCs."}
+variable "domain_name"          {                                       description = "AWS Route53 hosted zone name to be attached to the VPC." }
+variable "cidr_block"           { default     = "10.0.0.0/16"           description = "CIDR block assigned to the VPC." }
+variable "ipv6_cidr_block"      { default     = false                   description = "Enable IPv6." }
+variable "tenancy"              { default     = "default"               description = "Sets where the EC2 instance will run. Accepted values are 'default', 'dedicated' or 'host'." }
+variable "enable_dns_support"   { default     = true                    description = "Queries to the Amazon provided DNS server at the 169.254.169.253 IP address, or the reserved IP address at the base of the VPC IPv4 network range plus two will succeed." }
+variable "enable_dns_hostnames" { default     = true                    description = "Instances in the VPC get public DNS hostnames, but only if the enable_dns_support attribute is also set to true." }
+variable "domain_name_servers"  { default     = [ "AmazonProvidedDNS" ] description = "Sets the DNS server configured in the DHCP Options." }
+variable "AZs"                  { default     = 3                       description = "Number of Availability Zones we want to have." }
+variable "enable_vpc_flow_logs" { default     = false                   description = "Enable VPC Flow logs." }
+variable "enable_nat_gw"        { default     = true                    description = "Create one NAT Gateway for each AZs." }
